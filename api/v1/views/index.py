ea8d8ae5e462b8cab57568d4e5f8ed1afeba77ed
Certifieddonnie <mcdonaldsamure91@gmail.com>
2023-02-21 20:40:20 +0100

    Status of your API

---
+++ api/v1/views/index.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" routing File """
+from api.v1.views import app_views
+from flask import Flask, jsonify
+
+
+@app_views.route('/status', strict_slashes=False)
+def status():
+    """
+    Returns a JSON status
+    """
+    return jsonify({"status": "OK"})
