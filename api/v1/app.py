4a0230b8c6209026093710b231bd25673dd0251d
Certifieddonnie <mcdonaldsamure91@gmail.com>
2023-02-21 21:16:59 +0100

    Not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ app api """
 from models import storage
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from os import getenv
 from api.v1.views import app_views
 
@@ -14,6 +14,13 @@ def close(obj):
     """ calls close method """
     storage.close()
 
+
+@app.errorhandler(404)
+def not_found(error):
+    """ A custom 404 page not found """
+    return make_response(jsonify({
+        "error": "Not found"
+    }), 404)
 
 if __name__ == "__main__":
 
