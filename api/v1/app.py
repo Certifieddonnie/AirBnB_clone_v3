5b224bba3e19d40543c01d884ada5997d7ac01f5
Certifieddonnie <mcdonaldsamure91@gmail.com>
2023-02-22 12:24:05 +0100

    HTTP access control (CORS)

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,10 +3,12 @@
 from models import storage
 from flask import Flask, make_response, jsonify
 from os import getenv
+from flask_cors import CORS
 from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
@@ -21,6 +23,7 @@ def not_found(error):
     return make_response(jsonify({
         "error": "Not found"
     }), 404)
+
 
 if __name__ == "__main__":
 
